5f8471742a66d058e955f6b308faaf756153e759
Everistus Olumese <everistus.o@turing.com>
2020-04-20 13:23:10 +0100

    Building

---
+++ udacity-c3-deployment/k8s/setup.sh
@@ -0,0 +1,26 @@
+# /usr/bin/bash
+
+kubectl delete daemonsets,replicasets,services,deployments,pods,rc --all
+
+
+kubectl apply -f aws-secret.yaml
+kubectl apply -f env-secret.yaml
+kubectl apply -f env-configmap.yaml
+
+kubectl apply -f reverseproxy-deployment.yaml
+kubectl apply -f reverseproxy-service.yaml
+
+kubectl apply -f pod.yaml
+
+
+kubectl apply -f backend-feed-deployment.yaml
+kubectl apply -f backend-feed-service.yaml
+
+kubectl apply -f backend-user-deployment.yaml
+kubectl apply -f backend-user-service.yaml
+
+kubectl apply -f frontend-deployment.yaml
+kubectl apply -f frontend-service.yaml
+
+
+kubectl get pods
