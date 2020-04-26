840e42860f7965091574487dafc62c919404e558
Everistus Olumese <everistus.o@turing.com>
2020-04-26 13:48:41 +0100

    Solution

--- udacity-c3-deployment/k8s/setup.sh
+++ udacity-c3-deployment/k8s/setup.sh
@@ -7,10 +7,7 @@ kubectl apply -f aws-secret.yaml
 kubectl apply -f env-secret.yaml
 kubectl apply -f env-configmap.yaml
 
-kubectl apply -f reverseproxy-deployment.yaml
-kubectl apply -f reverseproxy-service.yaml
-
-kubectl apply -f pod.yaml
+# kubectl apply -f pod.yaml
 
 
 kubectl apply -f backend-feed-deployment.yaml
@@ -21,6 +18,10 @@ kubectl apply -f backend-user-service.yaml
 
 kubectl apply -f frontend-deployment.yaml
 kubectl apply -f frontend-service.yaml
+
+
+kubectl apply -f reverseproxy-deployment.yaml
+kubectl apply -f reverseproxy-service.yaml
 
 
 kubectl get pods
